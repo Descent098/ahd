7d3967630cb2d8978ce51b5f604c652f26e47f76
Kieran Wood <kieranw098@gmail.com>
2020-08-18 20:30:52 -0600

    Added ability to remove old config file

--- ahd/configuration.py
+++ ahd/configuration.py
@@ -12,7 +12,7 @@ def migrate_config():
     CONFIG_FILE_PATH = f"{os.path.dirname(__file__)}{os.sep}ahd.yml"
     OLD_CONFIG_FILE_PATH = f"{os.path.dirname(__file__)}{os.sep}.ahdconfig"
     if os.path.isfile(OLD_CONFIG_FILE_PATH):
-        print(f"{colored.fg(1)}Old Configuration file found in {OLD_CONFIG_FILE_PATH} automatically migrating to version 0.5.0+")
+        print(f"{colored.fg(1)}Old Configuration file found in {OLD_CONFIG_FILE_PATH} automatically migrating to version 0.5.0+{colored.fg(15)}")
         with open(OLD_CONFIG_FILE_PATH, "r") as old_config_file:
             old_config = ConfigParser()
             old_config.readfp(old_config_file)
@@ -25,7 +25,18 @@ def migrate_config():
         with open(CONFIG_FILE_PATH, "w") as new_config_file:
             yaml.dump(new_config, new_config_file, default_flow_style=False)
         del old_config
-        return True
+
+        valid = False
+        while not valid:
+            remove_legacy = input("Would you like to remove the old configuration file (y or n)?")
+            if remove_legacy.lower().startswith("y"):
+                os.remove(OLD_CONFIG_FILE_PATH)
+            elif remove_legacy.lower().startswith("n"):
+                return True
+            else:
+                print("Please enter Y to remove config or N to not")
+                continue
+
 
     else: # If no legacy configs are present
         return False
